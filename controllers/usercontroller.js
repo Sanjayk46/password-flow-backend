bcc2053c15e449015eeab7bfa1e8fa317d6bd9b0
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-02-03 00:59:31 +0530

    Cha url

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -159,7 +159,8 @@ const forgotPassword = async (req, res) => {
         <p>Dear ${user.firstName} ${user.lastName},</p>
         <p>We received a request to reset your password. Here is your One-Time Password (OTP): <strong>${OTP}</strong></p>
         <p>Please click the following link to reset your password:</p>
-        <a href="https://main--chipper-conkies-c7774d.netlify.app/reset-password">Reset Password</a>
+        //<a href="https://main--chipper-conkies-c7774d.netlify.app/reset-password">Reset Password</a>
+        <a href="https://password-reset-flows.netlify.app/reset-password">Reset Password</a>
         <p>If you did not make this request, please ignore this email.</p>
         <p>Thank you,</p>
         <p>From Validation</p>
